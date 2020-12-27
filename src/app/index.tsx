b1ca7c1ab61fede6ed8b99801c9552498455ded7
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-12-27 17:58:47 +0200

    Login form semi-ready

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -15,21 +15,23 @@ import { GlobalStyle } from 'styles/global-styles';
 import { HomePage } from './containers/HomePage/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
+import { Login } from './containers/Login/Loadable';
 
 export function App() {
   const { i18n } = useTranslation();
   return (
     <BrowserRouter>
       <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
+        titleTemplate="%s - BookShareBG"
+        defaultTitle="BookShareBG"
         htmlAttributes={{ lang: i18n.language }}
       >
-        <meta name="description" content="A React Boilerplate application" />
+        <meta name="description" content="A BookShareBG application" />
       </Helmet>
 
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={Login} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
